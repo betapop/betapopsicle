bc6a6be59ceaa15458d169d66a9f0deed41b0c26
betapop <88597878+betapop@users.noreply.github.com>
2025-10-19 15:48:37 -0400

    half of the index
    banner, calendar, waid, and navigation. also nav has a width now (noting incase it breaks something LOL)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./main.scss";
 import { tabTitle } from "./js/example.js";
 import { discordChecker } from "./js/discord.js";
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
